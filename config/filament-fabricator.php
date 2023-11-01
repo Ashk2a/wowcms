79889da1694755d39a8411269f6b1ea77c7666b0
Ashk <timed2a@hotmail.fr>
2023-11-01 18:18:15 +0100

    ✨ Move fabricator layout and block in default theme

--- config/filament-fabricator.php
+++ config/filament-fabricator.php
@@ -1,6 +1,5 @@
 <?php
 
-// config for Z3d0X/FilamentFabricator
 return [
     'routing' => [
         'enabled' => true,
@@ -24,24 +23,20 @@ return [
     ],
 
     'middleware' => [
-        \Illuminate\Cookie\Middleware\EncryptCookies::class,
-        \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
-        \Illuminate\Session\Middleware\StartSession::class,
-        // \Illuminate\Session\Middleware\AuthenticateSession::class,
-        \Illuminate\View\Middleware\ShareErrorsFromSession::class,
-        \Illuminate\Foundation\Http\Middleware\VerifyCsrfToken::class,
-        \Illuminate\Routing\Middleware\SubstituteBindings::class,
+        Illuminate\Cookie\Middleware\EncryptCookies::class,
+        Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
+        Illuminate\Session\Middleware\StartSession::class,
+        App\Http\Middleware\LoadAppTheme::class,
+        Illuminate\View\Middleware\ShareErrorsFromSession::class,
+        Illuminate\Foundation\Http\Middleware\VerifyCsrfToken::class,
+        Illuminate\Routing\Middleware\SubstituteBindings::class,
     ],
 
-    'page-model' => \Z3d0X\FilamentFabricator\Models\Page::class,
+    'page-model' => Z3d0X\FilamentFabricator\Models\Page::class,
 
-    'page-resource' => \App\Filament\Admin\Resources\PageResource::class,
+    'page-resource' => App\Filament\Admin\Resources\PageResource::class,
 
     'enable-view-page' => false,
 
-    /*
-     * This is the name of the table that will be created by the migration and
-     * used by the above page-model shipped with this package.
-     */
     'table_name' => 'pages',
 ];
